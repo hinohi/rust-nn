6d1f836a23693944dd3a8318bc5315a19f0c6e57
Daiju Nakayama <42.daiju@gmail.com>
2019-08-05 22:02:25 +0900

    examples/xorがxorじゃなかったので改名

--- examples/product.rs
+++ examples/product.rs
@@ -6,28 +6,33 @@ use rust_nn::train::*;
 fn main() {
     let mut random = Mcg128Xsl64::new(1);
 
-    let batch_size = 1000;
+    // batch size is fixed
+    let batch_size = 100;
+    // build a model, that has 1 input-layer (dim=2), 4 hidden-layers (dim=8,16,16,8)
     let mut model = NN4Regression::new(
-        [2, 8, 8, 8, 8],
+        [2, 8, 16, 16, 8],
         batch_size,
         Adam::default(),
         Adam::default(),
     );
 
+    // training loop
     for epoch in 1..=10000 {
         // make data
         let (x, t) = {
             let mut x = Vec::with_capacity(batch_size);
             let mut t = Vec::with_capacity(batch_size);
             for _ in 0..batch_size {
-                let a = random.gen_range(-1.0, 1.0);
-                let b = random.gen_range(-1.0, 1.0);
-                let c = if 0.0 < a * b { 1.0 } else { -1.0 };
+                let a = random.gen_range(-2.0, 2.0);
+                let b = random.gen_range(-2.0, 2.0);
+                let c = a * b;
                 x.push([a, b]);
                 t.push([c]);
             }
             (arr2(&x), arr2(&t))
         };
+        // train mini-batch and calc loss.
+        // LOSS of Regression model is MSE.
         let loss = model.train(&x, &t);
         println!("{} {}", epoch, loss);
     }
